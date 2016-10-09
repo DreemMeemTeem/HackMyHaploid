c95ab7fe8e71c930b0d8d056cc23eaf6e8e20a28
Jesse Friedman <jesse@jesse.ws>
2016-10-08 23:23:19 -0400

    Changing mutableCodon to use RNA codon instead of amino acid abbreviation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-
-
 var editor = new AminoEdit({
   element: document.getElementById('aminoedit'),
-  //basePairs: ['A', 'T', 'T', 'C', 'G', 'C', 'A', 'G'],
   basePairs: 'agcgcctacgctccccctacc',
   mutableNucleotideIndicies: [18, 19],
   events: {
-    onAminoUpdate: function(sequence) {
-      var mutableCodon = sequence[sequence.length - 1];
+    onRNAUpdate: function(sequence) {
+      var mutableCodon = sequence.slice(sequence.length - 3, sequence.length).join('');
       console.log(mutableCodon)
     }
   }
