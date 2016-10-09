5ebacca6aeebc01af6be98f769032f07b5176cb3
Jesse Friedman <jesse@jesse.ws>
2016-10-09 09:44:51 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 1556e1c48cf9917adafe78921eb706940c617ef1, reversing
    changes made to 94be8f0ba70851783f036bf8273837a75a3e7ed2.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,61 @@
+var directions = new DirectionsDisplayer({
+  element: $('#directions')
+});
 
+var indicator = new ProteinIndicator({
+  element: $('#proteinindicator'),
+  imageSrc: './img/p53.png'
+});
 
 var editor = new AminoEdit({
-  element: document.getElementById('aminoedit'),
-  //basePairs: ['A', 'T', 'T', 'C', 'G', 'C', 'A', 'G'],
+  element: $('#aminoedit'),
   basePairs: 'agcgcctacgctccccctacc',
   mutableNucleotideIndicies: [18, 19],
   events: {
+    onRNAUpdate: function(sequence) {
+      var mutableCodon = sequence.slice(sequence.length - 3).join('');
+      indicator.updateDisplay(mutableCodon);
+    },
     onAminoUpdate: function(sequence) {
-      var mutableCodon = sequence[sequence.length - 1];
-      var imgP;
-      switch(mutableCodon){
-          case "UGG":
-            imgP="";
-            break;
-          case "AGG":
-            imgP="";
-            break;
-          case "CGG":
-            imgP="";
-            break;
-          case "GGG":
-            imgP="";
-            break;
-          case "UCG":
-            imgP="";
-            break;
-          case "UAG":
-            imgP="";
-            break;
-          case "UUG":
-            imgP="";
-            break;
+      if (directions.isStage('intro')) {
+        var DNAcodon = editor.currentDNASequence.slice(editor.currentDNASequence.length - 3).join('');
+        var RNAcodon = editor.currentRNASequence.slice(editor.currentRNASequence.length - 3).join('');
+        directions.startStage('aminodone', {
+          DNA: editor.currentDNASequence,
+          DNAcodon: DNAcodon,
+          RNA: editor.currentRNASequence,
+          RNAcodon: RNAcodon,
+          amino: editor.currentAminoSequence,
+          aminoDesc: indicator.aminoHighlightMap[RNAcodon].description
+        });
       }
-      console.log(mutableCodon)
     }
+  },
+  directions: directions
+});
+
+var cell = new CellAnimation({
+  element: $('#cellanimation'),
+  sprites: {
+    healthy: './img/cell-healthy.png',
+    cancerous: './img/cell-cancerous.png',
+    virus: './img/virus.png'
   }
 });
+
+$('#proteininject').click(function() {
+  var sequence = editor.currentRNASequence;
+  var codon = editor.currentRNASequence.slice(editor.currentRNASequence.length - 3).join('');
+  
+  var callback = function() {
+    if (directions.isStage('sendoncovirus')) {
+      directions.startStage('oncovirusoverview');
+    }
+  }
+  
+  if (codon === 'UGG' && false) {   // we're no longer using this, as "send oncovirus" is conceptually separate from the DNA/RNA/amino acid assembly
+    cell.changeState('healthy', callback);
+  } else {
+    cell.changeState('cancerous', callback);
+  }
+})
